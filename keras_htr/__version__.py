9a94682d46d86fe5d45a4638cbe1c64461296856
Eugene <supernovaprotocol@gmail.com>
2020-04-06 11:14:36 +0700

    Bump a minor version

--- keras_htr/__version__.py
+++ keras_htr/__version__.py
@@ -1,5 +1,5 @@
 __name__ = 'keras_htr'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __author__ = 'Evgenii Dolotov'
 __author_email__ = 'supernovaprotocol@gmail.com'
 __description__ = 'A toolkit for developing handwritten text recognition (HTR) pipelines'
